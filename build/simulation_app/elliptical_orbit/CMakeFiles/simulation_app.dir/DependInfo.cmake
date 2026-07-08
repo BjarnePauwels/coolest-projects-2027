de2190fb50ac7b77846a3dbaacd5a3439e978b2a
BjarnePauwels <Bjarne.pauwels2011@gmail.com>
2026-07-08 21:13:22 +0200

    build elliptical orbit into an app

---
+++ build/simulation_app/elliptical_orbit/CMakeFiles/simulation_app.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/bjarnepauwels/Downloads/1-PROGRAMMING/coolest_projects/2027/simulation-app/Include/imgui/imgui.cpp" "CMakeFiles/simulation_app.dir/Include/imgui/imgui.cpp.o" "gcc" "CMakeFiles/simulation_app.dir/Include/imgui/imgui.cpp.o.d"
+  "/Users/bjarnepauwels/Downloads/1-PROGRAMMING/coolest_projects/2027/simulation-app/Include/imgui/imgui_demo.cpp" "CMakeFiles/simulation_app.dir/Include/imgui/imgui_demo.cpp.o" "gcc" "CMakeFiles/simulation_app.dir/Include/imgui/imgui_demo.cpp.o.d"
+  "/Users/bjarnepauwels/Downloads/1-PROGRAMMING/coolest_projects/2027/simulation-app/Include/imgui/imgui_draw.cpp" "CMakeFiles/simulation_app.dir/Include/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/simulation_app.dir/Include/imgui/imgui_draw.cpp.o.d"
+  "/Users/bjarnepauwels/Downloads/1-PROGRAMMING/coolest_projects/2027/simulation-app/Include/imgui/imgui_tables.cpp" "CMakeFiles/simulation_app.dir/Include/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/simulation_app.dir/Include/imgui/imgui_tables.cpp.o.d"
+  "/Users/bjarnepauwels/Downloads/1-PROGRAMMING/coolest_projects/2027/simulation-app/Include/imgui/imgui_widgets.cpp" "CMakeFiles/simulation_app.dir/Include/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/simulation_app.dir/Include/imgui/imgui_widgets.cpp.o.d"
+  "/Users/bjarnepauwels/Downloads/1-PROGRAMMING/coolest_projects/2027/simulation-app/Include/imgui/rlImGui.cpp" "CMakeFiles/simulation_app.dir/Include/imgui/rlImGui.cpp.o" "gcc" "CMakeFiles/simulation_app.dir/Include/imgui/rlImGui.cpp.o.d"
+  "/Users/bjarnepauwels/Downloads/1-PROGRAMMING/coolest_projects/2027/simulation-app/main.cpp" "CMakeFiles/simulation_app.dir/main.cpp.o" "gcc" "CMakeFiles/simulation_app.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/simulation_app/elliptical_orbit/CMakeFiles/simulation_app.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simulation_app.dir/Include/imgui/imgui.cpp.o"
+  "CMakeFiles/simulation_app.dir/Include/imgui/imgui.cpp.o.d"
+  "CMakeFiles/simulation_app.dir/Include/imgui/imgui_demo.cpp.o"
+  "CMakeFiles/simulation_app.dir/Include/imgui/imgui_demo.cpp.o.d"
+  "CMakeFiles/simulation_app.dir/Include/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/simulation_app.dir/Include/imgui/imgui_draw.cpp.o.d"
+  "CMakeFiles/simulation_app.dir/Include/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/simulation_app.dir/Include/imgui/imgui_tables.cpp.o.d"
+  "CMakeFiles/simulation_app.dir/Include/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/simulation_app.dir/Include/imgui/imgui_widgets.cpp.o.d"
+  "CMakeFiles/simulation_app.dir/Include/imgui/rlImGui.cpp.o"
+  "CMakeFiles/simulation_app.dir/Include/imgui/rlImGui.cpp.o.d"
+  "CMakeFiles/simulation_app.dir/main.cpp.o"
+  "CMakeFiles/simulation_app.dir/main.cpp.o.d"
+  "simulation_app"
+  "simulation_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simulation_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
